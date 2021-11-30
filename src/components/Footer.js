ad05e6b8e25486f3e178662ca8e0fb8901ceaba0
Saikat <saikat.srimani@gmail.com>
2021-11-30 11:53:10 +0530

    added github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import facebook from "../assets/facebook.png"
 import instagram from "../assets/instagram.png"
 import twitter from "../assets/twitter.png"
 import reddit from "../assets/reddit.png"
+import git from "../assets/github.png"
 
 export default function Footer() {
     return (
@@ -11,6 +12,7 @@ export default function Footer() {
         <a href="https://www.facebook.com/saikat.srimani" ><img src={facebook} alt=" click here to check my facebook"></img></a>
         <a href="https://www.instagram.com/saikatsrimani/" ><img src={instagram} alt=" click here to check my instagram"></img></a>
         <a href="https://www.reddit.com/user/saiOBITO" ><img src={reddit} alt=" click here to check my reddit"></img></a>
+        <a href="https://github.com/saiOBITO" ><img src={git} alt=" click here to check my github"></img></a>
       </div>
     );
   }
